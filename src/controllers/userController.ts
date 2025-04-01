10f8dbb3ec1bc4e10012bbaed5ac8316ede23a7a
Efe Tuna <efetunagunayyy@gmail.com>
2025-04-01 14:43:05 +0300

    test2

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -9,7 +9,7 @@ export class UserController {
       if (!tcKimlikNo || !ad || !soyad ||!dogumYili ) {
         res.status(400).json({ message: "Tüm alanlar gereklidir!" });
       }
-
+  console.clear();
       const user = await UserService.registerUser(tcKimlikNo, ad, soyad, dogumYili, parola);
       res.status(201).json({ message: "Kullanıcı başarıyla kaydedildi!", user });
     } catch (error: any) {
