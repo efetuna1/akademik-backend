1119c855ab81e7b21737f6dfeefc78c2acbc51c3
Efe Tuna <efetunagunayyy@gmail.com>
2025-04-05 00:19:31 +0300

    login and register

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,50 @@
-import { Request, Response } from "express";
-import { UserService } from "../services/userService";
+// controllers/adayController.ts
+import { Request, Response } from 'express';
+import { createUser, loginUser } from '../services/userService';
+import jwt from 'jsonwebtoken';
 
-export class UserController {
-  static async register(req: Request, res: Response) {
+export const createUserController = async (req: Request, res: Response) => {
+  try {
+    const { tcKimlikNo, ad, soyad, parola } = req.body;
+    const yeniAday = await createUser(tcKimlikNo, ad, soyad, parola);
+
+    res.status(201).json({
+      mesaj: 'Aday başarıyla kaydedildi',
+      aday: yeniAday
+    });
+  } catch (error: any) {
+    res.status(400).json({ hata: error.message });
+  }
+};
+
+export const loginUserControler = async (req:Request , res:Response) =>{
+
+  const { tcKimlikNo, parola } = req.body;
+
+    if (!tcKimlikNo || !parola) {
+      res.status(400).json({ error: 'Eksik alanlar' });
+    }
+
     try {
-      const { tcKimlikNo, ad, soyad, dogumYili, parola } = req.body;
+      const loginResult = await loginUser(tcKimlikNo, parola);
 
-      if (!tcKimlikNo || !ad || !soyad ||!dogumYili ) {
-        res.status(400).json({ message: "Tüm alanlar gereklidir!" });
+      if (loginResult.error) {
+        res.status(400).json({ error: loginResult.error });
       }
-      const user = await UserService.registerUser(tcKimlikNo, ad, soyad, dogumYili, parola);
-      res.status(201).json({ message: "Kullanıcı başarıyla kaydedildi!", user });
-    } catch (error: any) {
-      res.status(400).json({ message: error.message });
+
+      const token = jwt.sign(
+        { userId: loginResult.user.id, tcKimlikNo: loginResult.user.tcKimlikNo }, 
+        process.env.JWT_SECRET, 
+        { expiresIn: '1h' } 
+      );
+      res.status(200).json({
+        message: loginResult.message,
+        user: loginResult.user,
+        token: token,  
+      });
+    } catch (error) {
+      res.status(500).json({
+        error: 'Login işlemi sırasında bir hata oluştu',
+      });
     }
   }
-}
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,17 +1,50 @@
-import { IdentityService } from "./soapService";
-import  prisma  from "../utils/prisma"; 
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+const prisma = new PrismaClient();
 
-export class UserService {
-  static async registerUser(tcKimlikNo: string, ad: string, soyad: string, dogumYili: number, parola: string) {
-    const isVerified = await IdentityService.verifyIdentity(tcKimlikNo, ad, soyad, dogumYili);
-
-    if (!isVerified) {
-      throw new Error("Kimlik doğrulama başarısız! Kayıt yapılamadı.");
-    }
-    const user = await prisma.kullanici.create({
-      data: { tcKimlikNo, ad, soyad, parola },
+export async function createUser(tcKimlikNo: string, ad: string, soyad: string, parola: string) {
+    const hashedPassword = await bcrypt.hash(parola, 10)
+    try {
+    const newUser = await prisma.kullanici.create({
+      data: {
+        tcKimlikNo:tcKimlikNo,
+        ad:ad,
+        soyad:soyad,
+        parola:hashedPassword
+      },
     });
-
-    return user;
+    console.log('Kullanıcı başarıyla kaydedildi:', newUser);
+  } catch (error) {
+    console.error('Kullanıcı kaydedilemedi:', error);
   }
 }
+
+export async function loginUser(tcKimlikNo: string, parola: string) {
+  try {
+    const user = await prisma.kullanici.findUnique({
+      where: {
+        tcKimlikNo: tcKimlikNo,
+      },
+    });
+
+    if (!user) {
+      console.log('Kullanıcı bulunamadı.');
+      return { error: 'Kullanıcı bulunamadı' };
+    }
+
+    const isPasswordValid = await bcrypt.compare(parola, user.parola);
+
+    if (!isPasswordValid) {
+      console.log('Parola geçersiz');
+      return { error: 'Geçersiz parola' };
+    }
+
+    console.log('Giriş başarılı:', user);
+    return { message: 'Giriş başarılı', user };
+
+  } catch (error) {
+    console.error('Giriş işlemi sırasında bir hata oluştu:', error);
+    return { error: 'Bir hata oluştu' };
+  }
+}
+
