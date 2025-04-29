abfef1a549ed719ec5f488944e6c1a29e4de909c
Efe Tuna <efetunagunayyy@gmail.com>
2025-04-29 15:39:50 +0300

    cv

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,29 @@
 import { Router } from "express";
 import { createIlan } from "../controllers/ilanController";
 import { basvuruYap } from "../controllers/basvuruController";
-import { createProje } from "../controllers/projeController";
-import { createHakem } from "../controllers/hakemController";
-import { createOdul } from "../controllers/odulController";
-import { createGorev } from "../controllers/gorevController";
-import { createSanat } from "../controllers/sanatController";
 import { getIlanlar } from "../controllers/ilanController";
 import { getTotalPuan } from "../controllers/toplamPuanlama";
 import { getJuriBasvurular } from "../controllers/juriController";
 import { validateIdentityController } from "../controllers/tcVerifyController";
+import { createUserController, loginUserControler } from "../controllers/userController";
+
+import { createMakale, getMakaleler } from "../controllers/makaleController";
+import { getToplantilar, createToplanti } from "../controllers/toplantiController";
+import { createFaaliyet, getEgitimFaaliyetleri } from "../controllers/faaliyetController";
+import { createKitap, getKitaplar } from "../controllers/kitapController";
+import { createAtif, getAtiflar } from "../controllers/atifController";
+import { createTez, getTezler } from "../controllers/tezController";
+import { createPatent, getPatentler } from "../controllers/patentController";
+import { createProje, getProjeler } from "../controllers/projeController";
+import { createHakem, getHakemlikler } from "../controllers/hakemController";
+import { createOdul, getOduller } from "../controllers/odulController";
+import { createGorev, getGorevler } from "../controllers/gorevController";
+import { createSanat, getSanatFaaliyetleri } from "../controllers/sanatController";
+import { getAdayCV } from "../controllers/adaycvController";
+
+
+
+
 
 
 const router = Router();
@@ -34,11 +48,25 @@ router.post("/gorevEkle", createGorev);
 router.post("/hakemEkle", createHakem);
 router.post("/odulEkle", createOdul);
 router.post("/kimlikDogrula", validateIdentityController);
-
 router.post("/basvuruYap", basvuruYap);
 
 router.get("/ilanGetir", getIlanlar);
 router.get("/toplamPuan", getTotalPuan);
 router.get("/juriBasvuru", getJuriBasvurular);
+
+router.get("/adayMakaleler", getMakaleler);
+router.get("/adayToplantilar", getToplantilar);
+router.get("/adayKitaplar", getKitaplar);
+router.get("/adayAtiflar", getAtiflar);
+router.get("/adayFaaliyetler", getEgitimFaaliyetleri);
+router.get("/adayTezler", getTezler);
+router.get("/adayPatentler", getPatentler);
+router.get("/adayProjeler", getProjeler);
+router.get("/adayHakemlikler", getHakemlikler);
+router.get("/adayOduller", getOduller);
+router.get("/adayGorevler", getGorevler);
+router.get("/adaySanatFaaliyetleri", getSanatFaaliyetleri);
+router.get("/adaycv", getAdayCV);
+
 
 export default router;
